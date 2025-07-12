3a71de901b76c4c1d411259e7a2dd315ad222fe7
Buroni123 <83972181+Buroni123@users.noreply.github.com>
2025-07-12 11:18:57 +0200

    Add

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("Hello World!")
+
+function getComputerChoice() {
+    const random = Math.floor(Math.random() * 3)
+    if (random === 0) {
+        return "Rock"
+    } else if (random === 1) {
+        return "Paper"
+    } else if (random === 2) {
+        return "Scissors"
+    }
+}
+
+console.log(getComputerChoice())
+
+// get 3 values
+// choose them randomly
