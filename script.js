8240fdcb4de0de210dcb5f7b5c341af59d644e82
Buroni123 <83972181+Buroni123@users.noreply.github.com>
2025-07-12 12:24:49 +0200

    Add playGame function and scores
    Added playGame function which consists of 5 rounds and then at the end tells the scores

--- script.js
+++ script.js
@@ -29,20 +29,37 @@ function playRound(humanChoice, computerChoice) {
         console.log("Draw! Both chose scissors")
     } else if (humanChoice === "rock" && computerChoice === "paper") {
         console.log("You lose! Paper beats rock")
+        computerScore++
     } else if (humanChoice === "paper" && computerChoice === "scissors") {
         console.log("You lose! Scissors beats paper")
+        computerScore++
     } else if (humanChoice === "scissors" && computerChoice === "rock") {
         console.log("You lose! Rock beats scissors")
+        computerScore++
     } else if (humanChoice === "rock" && computerChoice === "scissors") {
         console.log("You win! Rock beats scissors")
+        humanScore++
     } else if (humanChoice === "paper" && computerChoice === "rock") {
         console.log("You win! Paper beats rock")
+        humanScore++
     } else if (humanChoice === "scissors" && computerChoice === "paper") {
         console.log("You win! Scissors beats paper")
+        humanScore++
     }
 }
 
-const humanSelection = getHumanChoice()
-const computerSelection = getComputerChoice()
 
-playRound(humanSelection, computerSelection)
+function playGame() {
+    humanScore = 0
+    computerScore = 0
+    for(let i=0; i<5; i++) {
+        const humanSelection = getHumanChoice()
+        const computerSelection = getComputerChoice()
+        playRound(humanSelection, computerSelection)
+    }
+    console.log(`Human score: ${humanScore}, Computer score: ${computerScore}`)
+}
+
+
+
+playGame()
